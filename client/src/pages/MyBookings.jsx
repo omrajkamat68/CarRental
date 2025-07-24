19eab5bf65703e2246aa110f8e6a8439ce369c47
omrajkamat68 <ob2614@srmist.edu.in>
2025-07-24 10:37:25 +0530

    Booking info added and my bookings page completed.

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { dummyMyBookingsData } from '../assets/assets'
+import { assets, dummyMyBookingsData } from '../assets/assets'
 import Title from '../components/Title'
 
 const MyBookings = () => {
 
   const [bookings, setBookings] = useState([])
+  const currency = import.meta.env.VITE_CURRENCY
 
   const fetchMyBookings = async () => {
     setBookings(dummyMyBookingsData)
@@ -26,6 +27,7 @@ const MyBookings = () => {
         {bookings.map((booking, index)=>(
           <div key={booking._id} className='grid grid-cols-1 md:grid-cols-4 gap-6 p-6 border border-borderColor rounded-lg mt-5 first:mt-12'>
             {/* Car Image + Info */}
+
             <div className='md:col-span-1'>
               <div className='rounded-md overflow-hidden mb-3'>
                 <img src={booking.car.image} alt="" className='w-full h-auto aspect-video object-cover' />
@@ -34,6 +36,40 @@ const MyBookings = () => {
 
               <p className='text-gray-500'>{booking.car.year} • {booking.car.category} • {booking.car.location}</p>
             </div>
+
+            {/* Booking Info */}
+            <div className='md:col-span-2'>
+              <div className='flex items-center gap-2'>
+                <p className='px-3 py-1.5 bg-light rounded'>Booking #{index+1}</p>
+                <p className={`px-3 py-1 text-xs rounded-full ${booking.status === 'confirmed' ? 'bg-green-400/15 text-green-600' : 'bg-red-400/15 text-red-600'}`}>{booking.status}</p>
+              </div>
+
+              <div className='flex items-start gap-2 mt-3'>
+                <img src={assets.calendar_icon_colored} alt="" className='w-4 h-4 mt-1' />
+                <div>
+                  <p className='text-gray-500'>Rental Period</p>
+                  <p>{booking.pickupDate.split('T')[0]} To {booking.returnDate.split('T')[0]}</p>
+                </div>
+              </div>
+
+              <div className='flex items-start gap-2 mt-3'>
+                <img src={assets.location_icon_colored} alt="" className='w-4 h-4 mt-1' />
+                <div>
+                  <p className='text-gray-500'>Pick-up Location</p>
+                  <p>{booking.car.location}</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Price */}
+            <div className='md:col-span-1 flex flex-col justify-between gap-6'>
+              <div className='text-sm text-gray-500 text-right'>
+                <p>Total Price</p>
+                <h1 className='text-2xl font-semibold text-primary'>{currency}{booking.price}</h1>
+                <p>Booked on {booking.createdAt.split('T')[0]}</p>
+              </div>
+            </div>
+
           </div>
         ))}
        </div>
