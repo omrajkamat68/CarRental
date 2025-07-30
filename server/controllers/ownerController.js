47c7bcb71baa7b0b6ffa05dd53fda79eda9c34cb
omrajkamat68 <ob2614@srmist.edu.in>
2025-07-30 09:35:41 +0530

    Image kit config added.

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -10,3 +10,15 @@ export const changeRoleToOwner = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// API to list car
+export const addCar = async (req, res) => {
+  try {
+    const {_id} = req.user
+    let car = JSON.parse(req.body.carData)
+    const imageFile = req.file;
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+}
