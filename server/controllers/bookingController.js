030596a042dc2e8dfd28cf6d54d79d993641bce6
omrajkamat68 <ob2614@srmist.edu.in>
2025-08-02 12:04:59 +0530

    Controller function to check availability of cars for the given date and location added.

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,4 +1,5 @@
 import Booking from "../models/Booking.js";
+import Car from "../models/Car.js";
 
 // Function to check availability of car for a given date
 const checkAvailability = async (car, pickupDate, returnDate) => {
@@ -8,4 +9,32 @@ const checkAvailability = async (car, pickupDate, returnDate) => {
     returnDate: { $gte: pickupDate },
   });
   return bookings.length === 0;
+};
+
+// API to check availability of cars for the given date and location
+export const checkAvailabilityOfCar = async (req, res) => {
+  try {
+    const { location, pickupDate, returnDate } = req.body;
+
+    // fetch all available cars for the given location
+    const cars = await Car.find({ location, isAvailable: true });
+
+    // check car availability for the given date range using promise
+    const availableCarsPromises = cars.map(async (car) => {
+      const isAvailable = await checkAvailability(
+        car._id,
+        pickupDate,
+        returnDate
+      );
+      return { ...car._doc, isAvailable: isAvailable };
+    });
+
+    let availableCars = await Promise.all(availableCarsPromises);
+    availableCars = availableCars.filter((car) => car.isAvailable === true);
+
+    res.json({ success: true, availableCars });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
 };
