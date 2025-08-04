4f1806561521e0a3259f6f0d8aef0379d4edd11b
omrajkamat68 <ob2614@srmist.edu.in>
2025-08-04 09:39:41 +0530

    Controller function to get dashboard data added.

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,4 +1,5 @@
 import imagekit from "../configs/imageKit.js";
+import Booking from "../models/Booking.js";
 import Car from "../models/Car.js";
 import User from "../models/User.js";
 import fs from "fs";
@@ -101,6 +102,51 @@ export const deleteCar = async (req, res) => {
     await car.save();
 
     res.json({ success: true, message: "Car Removed" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to get dashboard data
+export const getDashboardData = async (req, res) => {
+  try {
+    const { _id, role } = req.user;
+
+    if (role !== "owner") {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+
+    const cars = await Car.find({ owner: _id });
+    const bookings = await Booking.find({ owner: _id })
+      .populate("car")
+      .sort({ createdAt: -1 });
+
+    const pendingBookings = await Booking.find({
+      owner: _id,
+      status: "pending",
+    });
+    const completedBookings = await Booking.find({
+      owner: _id,
+      status: "confirmed",
+    });
+
+    // Calculate monthlyRevenue from bookings where status is confirmed
+    const monthlyRevenue = bookings
+      .slice()
+      .filter((booking) => booking.status === "confirmed")
+      .reduce((acc, booking) => acc + booking.price, 0);
+
+    const dashboardData = {
+      totalCars: cars.length,
+      totalBookings: bookings.length,
+      pendingBookings: pendingBookings.length,
+      completedBookings: completedBookings.length,
+      recentBookings: bookings.slice(0, 3),
+      monthlyRevenue,
+    };
+
+    res.json({ success: true, dashboardData });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -4,6 +4,7 @@ import {
   addCar,
   changeRoleToOwner,
   deleteCar,
+  getDashboardData,
   getOwnerCars,
   toggleCarAvailability,
 } from "../controllers/ownerController.js";
@@ -16,5 +17,7 @@ ownerRouter.post("/add-car", upload.single("image"), protect, addCar);
 ownerRouter.get("/cars", protect, getOwnerCars);
 ownerRouter.post("/toggle-car", protect, toggleCarAvailability);
 ownerRouter.post("/delete-car", protect, deleteCar);
+
+ownerRouter.get("/dashboard", protect, getDashboardData);
 
 export default ownerRouter;
