b2b201b56ee407b3da6f3874cbe19029d1f784c6
omrajkamat68 <ob2614@srmist.edu.in>
2025-08-04 09:16:35 +0530

    Controller function to change booking status added.

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -88,15 +88,39 @@ export const getUserBookings = async (req, res) => {
   }
 };
 
-
 // API to get owner bookings
 export const getOwnerBookings = async (req, res) => {
   try {
-    if(req.user.role !== 'owner'){
-      return res.json({ success: false, message: "Unauthorized" })
+    if (req.user.role !== "owner") {
+      return res.json({ success: false, message: "Unauthorized" });
     }
-    const bookings = await Booking.find({owner: req.user._id}).populate('car user').select("-user.password").sort({createdAt: -1})
+    const bookings = await Booking.find({ owner: req.user._id })
+      .populate("car user")
+      .select("-user.password")
+      .sort({ createdAt: -1 });
     res.json({ success: true, bookings });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to change booking status
+export const changeBookingStatus = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { bookingId, status } = req.body;
+
+    const booking = await Booking.findById(bookingId);
+
+    if (booking.owner.toString() !== _id.toString()) {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+
+    booking.status = status;
+    await booking.save();
+
+    res.json({ success: true, message: "Status Updated" });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
