17f643510d0a84aef691c155dacf642902bee3b1
omrajkamat68 <ob2614@srmist.edu.in>
2025-08-05 09:47:13 +0530

    Function to fetch all cars from the server and to logout the user added.

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,70 +1,104 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import axios from 'axios'
-import {toast} from 'react-hot-toast'
+import axios from "axios";
+import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 // eslint-disable-next-line react-refresh/only-export-components
-export const AppContext = createContext()
+export const AppContext = createContext();
 
-export const AppProvider = ({ children })=>{
+export const AppProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const currency = import.meta.env.VITE_CURRENCY;
 
-    const navigate = useNavigate()
-    const currency = import.meta.env.VITE_CURRENCY
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [isOwner, setIsOwner] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [pickupDate, setPickupDate] = useState("");
+  const [returnDate, setReturnDate] = useState("");
 
-    const [token, setToken] = useState(null);
-    const [user, setUser] = useState(null);
-    const [isOwner, setIsOwner] = useState(false);
-    const [showLogin, setShowLogin] = useState(false);
-    const [pickupDate, setPickupDate] = useState('');
-    const [returnDate, setReturnDate] = useState('');
+  const [cars, setCars] = useState([]);
 
-    const [cars, setCars] = useState([])
-
-    // Function to check if user is logged in
-    const fetchUser = async () => {
-        try {
-            const {data} = await axios.get('/api/user/data')
-            if (data.success) {
-                setUser(data.user)
-                setIsOwner(data.user.role === 'owner')
-            }else{
-                navigate('/')
-            }
-        } catch (error) {
-            toast.error(error.message)
-        }   
+  // Function to check if user is logged in
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios.get("/api/user/data");
+      if (data.success) {
+        setUser(data.user);
+        setIsOwner(data.user.role === "owner");
+      } else {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
-    // useEffect to retrieve the token from localstorage
-    useEffect(()=>{
-        const token = localStorage.getItem('token')
-        setToken(token)
-    },[])
+  // Function to fetch all cars from the server
+  const fetchCars = async () => {
+    try {
+      const { data } = await axios.get("/api/user/cars");
+      data.success ? setCars(data.cars) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
-    // useEffect to fetch user data when token is available
-    useEffect(()=>{
-        if(token){
-            axios.defaults.headers.common['Authorization'] = `${token}`
-            fetchUser()
-        }
+  // Function to logout the user
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    setUser(null);
+    setIsOwner(false);
+    axios.defaults.headers.common["Authorization"] = "";
+    toast.success("You have been logged out");
+  };
+
+  // useEffect to retrieve the token from localstorage
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setToken(token);
+    fetchCars();
+  }, []);
+
+  // useEffect to fetch user data when token is available
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = `${token}`;
+      fetchUser();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[token])
+  }, [token]);
 
-    const value = {
-        navigate, currency
-    }
+  const value = {
+    navigate,
+    currency,
+    axios,
+    user,
+    setUser,
+    token,
+    setToken,
+    isOwner,
+    setIsOwner,
+    fetchUser,
+    showLogin,
+    setShowLogin,
+    logout,
+    fetchCars,
+    cars,
+    setCars,
+    pickupDate,
+    setPickupDate,
+    returnDate,
+    setReturnDate,
+  };
 
-    return (
-        <AppContext.Provider value={value}>
-            { children }
-        </AppContext.Provider>
-    )
-
-}
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
 
 // eslint-disable-next-line react-refresh/only-export-components
-export const useAppContext = ()=>{
-    return useContext(AppContext)
-}
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
