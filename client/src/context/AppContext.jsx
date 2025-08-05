93870a30ea4a424a68370bbdcc41b5eb43a45f8d
omrajkamat68 <ob2614@srmist.edu.in>
2025-08-05 09:28:33 +0530

    Context added

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState } from "react";
+import axios from 'axios'
+import {toast} from 'react-hot-toast'
+import { useNavigate } from "react-router-dom";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AppContext = createContext()
+
+export const AppProvider = ({ children })=>{
+
+    const navigate = useNavigate()
+    const currency = import.meta.env.VITE_CURRENCY
+
+    const [token, setToken] = useState(null);
+    const [user, setUser] = useState(null);
+    const [isOwner, setIsOwner] = useState(false);
+    const [showLogin, setShowLogin] = useState(false);
+    const [pickupDate, setPickupDate] = useState('');
+    const [returnDate, setReturnDate] = useState('');
+
+
+    const value = {
+        navigate, currency
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            { children }
+        </AppContext.Provider>
+    )
+
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAppContext = ()=>{
+    return useContext(AppContext)
+}
