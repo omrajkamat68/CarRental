2a09b0becf753c5c8c19d4e8f1b9d247ea8114b7
omrajkamat68 <ob2614@srmist.edu.in>
2025-08-11 09:39:17 +0530

    Functionality to fetch bookings added.

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { assets, dummyCarData } from '../assets/assets'
+import { assets } from '../assets/assets'
 import Loader from '../components/Loader'
 import { useAppContext } from '../context/AppContext'
 import toast from 'react-hot-toast'
